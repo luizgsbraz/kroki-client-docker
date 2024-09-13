754ff60d4cb52ca0b06dfeaee1353f19ba3f63c2
Luiz Braz <luiz.lgsb@somewhere.com>
2024-09-13 20:19:32 -0300

    ajusta build automatizado

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-docker build --no-cache -t kroki-client .
+# docker build --no-cache -t kroki-client .
+
+TAG=$(date +%s)
+docker build --no-cache . --file Dockerfile --tag luizgsbraz/kroki-client:$TAG
+docker tag luizgsbraz/kroki-client:$TAG luizgsbraz/kroki-client:latest
