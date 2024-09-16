fcac54e411878dec54330c4e392e68773336f87a
luiz braz <luiz@anyrhwhere.com>
2024-09-16 11:08:44 -0400

    adapta para geração com podman

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
-# docker build --no-cache -t kroki-client .
+# $CONTAINER_TOOL build --no-cache -t kroki-client .
+# Verifica se o Podman está disponível, caso contrário, usa Docker
+if command -v podman &> /dev/null
+then
+    export CONTAINER_TOOL="podman"
+else
+    export CONTAINER_TOOL="docker"
+fi
 
 TAG=$(date +%s)
-docker build --no-cache . --file Dockerfile --tag luizgsbraz/kroki-client:$TAG
-docker tag luizgsbraz/kroki-client:$TAG luizgsbraz/kroki-client:latest
+$CONTAINER_TOOL build --no-cache . --file Dockerfile --tag luizgsbraz/kroki-client:$TAG
+$CONTAINER_TOOL tag luizgsbraz/kroki-client:$TAG luizgsbraz/kroki-client:latest
